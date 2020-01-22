11874ac7c4e9c4a344cad5989897dadfea8c5711
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2020-01-22 10:24:24 +0100

    first commit

---
+++ Boletin_16.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_16;
+
+/**
+ *
+ * @author jalonsoriveiro
+ */
+public class Boletin_16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        IPodeCantar perso1 =  new Persoa();
+        perso1.cantar();
+                
+        IPodeCantar canario = new Canario();
+        canario.cantar();
+        IPodeCantar Galo = new Galo();
+        Galo.cantar();
+        
+    }
+    
+}
---
+++ IPodeCantar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_16;
+
+/**
+ *
+ * @author jalonsoriveiro
+ */
+public interface IPodeCantar {
+    
+
+      public abstract void cantar();
+      
+}
